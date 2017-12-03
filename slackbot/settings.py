cf3189b258e99ef75ad55278beaa299d34529937
SlavaKyrai <slavakyrai@gmail.com>
2017-12-03 16:52:35 +0200

    Update requirements

--- slackbot/settings.py
+++ slackbot/settings.py
@@ -123,3 +123,6 @@ SLACK_VERIFICATION_TOKEN = 'LrsY57cPk5zEmajfo5th5INY'
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 DATABASES['default']['CONN_MAX_AGE'] = 500
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
